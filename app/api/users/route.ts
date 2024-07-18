e83021ab4d9924109660407e310975253531ff7d
mokatildev <mokatildev@gmail.com>
2024-07-18 11:24:56 +0100

    done

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,10 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs"
 
-const prisma = new PrismaClient()
-const slatRounds = 10;
-
 export async function POST(req: NextRequest) {
+
+    const prisma = new PrismaClient()
+    const slatRounds = 10;
+
     const body = await req.json();
     const { email, password, username, bio } = body;
 
